2edccecf96c123201b3451cdd32292e164592946
dhanifery <feryr4@gmail.com>
2022-06-05 20:57:17 +0700

    database update

--- database/db_kursus.sql
+++ database/db_kursus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 23, 2022 at 10:25 PM
+-- Generation Time: Jun 05, 2022 at 08:55 PM
 -- Server version: 8.0.29-0ubuntu0.20.04.3
 -- PHP Version: 7.4.3
 
@@ -57,7 +57,7 @@ CREATE TABLE `tbl_instruktur` (
   `TTL` varchar(50) DEFAULT NULL,
   `no_telp` varchar(20) DEFAULT NULL,
   `JK` enum('Male','Female') DEFAULT NULL,
-  `honor` int DEFAULT NULL,
+  `honor` varchar(100) DEFAULT NULL,
   `image_instr` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `deskripsi_instr` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -67,9 +67,9 @@ CREATE TABLE `tbl_instruktur` (
 --
 
 INSERT INTO `tbl_instruktur` (`id_instruktur`, `id_user`, `username_instr`, `email_instr`, `TTL`, `no_telp`, `JK`, `honor`, `image_instr`, `deskripsi_instr`) VALUES
-(12, 20, 'Rahmat', 'rahmat@gmail.com', '1997-04-04', '0812', 'Male', 50000, 'img1652808201.jpg', 'sudah berpengalaman selama 12 tahun'),
-(16, NULL, 'Joko', 'joko@gmail.com', '1990-12-08', '0812', 'Male', 50000, 'img1653313539.jpg', 'halooo saya joko, semoga hari anda menyenangkan...'),
-(17, 29, 'Pamali', 'pamali@gmail.com', '1999-09-09', '0812', 'Male', 50000, 'img1653317261.png', 'Hai');
+(12, 30, 'Robert', 'rahmat@gmail.com', '1997-04-04', '0812', 'Male', '50000', 'img1652808201.jpg', 'sudah berpengalaman selama 12 tahun'),
+(16, 31, 'Budi', 'budi@gmail.com', '1990-12-08', '0812', 'Male', '50000', 'img1653313539.jpg', 'halooo saya budi, semoga hari anda menyenangkan...'),
+(17, 29, 'Ahmad K5', 'pamali@gmail.com', '1999-09-09', '081027654978', 'Male', 'Rp.55,000', 'img1654420571.jpg', 'Hai salam kenal, semoga harimu menyenangkan...');
 
 -- --------------------------------------------------------
 
@@ -102,7 +102,28 @@ CREATE TABLE `tbl_jadwal` (
 --
 
 INSERT INTO `tbl_jadwal` (`id_jadwal`, `id_user_peserta`, `id_user_instruktur`, `id_peserta`, `id_instruktur`, `id_paket`, `kode_jadwal`, `tgl_jadwal`, `tgl_latihan`, `jam_latihan`, `total_bayar`, `atas_nama`, `bank`, `bukti_bayar`, `no_rek`, `status_jadwal`, `status_bayar`) VALUES
-(21, 28, 29, 17, 17, 9, '20220523PBXAC', '1653317040', '2022-12-12', '21:01', '650000', 'Joko', 'BCA', 'img1653317365.jpg', '1234-1234-1234-1234', 2, 1);
+(30, 28, 29, 17, 17, 8, '20220605ZIQSU', '2022-06-05', '2022-06-22', '10:00 - 12:00', '550000', 'Gracia', 'BCA', 'img1654431561.png', '1231-1232-1234-1234', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kantor`
+--
+
+CREATE TABLE `tbl_kantor` (
+  `id_kantor` int NOT NULL,
+  `alamat` varchar(123) DEFAULT NULL,
+  `no_telp` varchar(100) DEFAULT NULL,
+  `deskripsi` text,
+  `image` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_kantor`
+--
+
+INSERT INTO `tbl_kantor` (`id_kantor`, `alamat`, `no_telp`, `deskripsi`, `image`) VALUES
+(1, 'Jln.Cut Mutia No.1', '081290909876', 'BISTIR merupakan aplikasi pendaftaran online kursus mobil', 'img1654355600.jpg');
 
 -- --------------------------------------------------------
 
@@ -150,7 +171,7 @@ CREATE TABLE `tbl_paket` (
 --
 
 INSERT INTO `tbl_paket` (`id_paket`, `id_mobil`, `nama_paket`, `byk_pertemuan`, `harga`, `deskripsi_paket`, `image`) VALUES
-(7, 7, 'Paket 1', '8 Pertemuan', '450000', 'Paket ini terdiri dari 8 pertemuan  selamat datang di website kami\r\n', 'img1652716932.png'),
+(7, 7, 'Paket 1', '8 Pertemuan', '450000', 'Paket ini terdiri dari 8 pertemuan  selamat datang di website kami\r\n', 'img1654424869.jpg'),
 (8, 6, 'Paket 2 ', '10 Pertemuan', '550000', 'Paket ini terdiri dari 10 pertemuan', 'img1652715207.jpeg'),
 (9, 5, 'Paket 3', '12 Pertemuan', '650000', 'Paket ini terdiri dari 12 pertemuan', 'img1652715259.jpeg');
 
@@ -177,7 +198,11 @@ CREATE TABLE `tbl_peserta` (
 --
 
 INSERT INTO `tbl_peserta` (`id_peserta`, `id_user`, `username_peserta`, `email_peserta`, `TTL`, `alamat`, `no_telp`, `JK`, `image_peserta`) VALUES
-(17, 28, 'joko', 'joko@gmail.com', '1999-12-09', 'Bekasi', '0812', 'Male', 'img1653317014.png');
+(17, 28, 'joko', 'joko@gmail.com', '1999-12-09', 'Bekasi', '0812', 'Male', 'img1653317014.png'),
+(18, 32, 'akun', 'akun@gmail.com', '2022-12-09', 'bekasi', '0812', 'Male', 'img1653384132.png'),
+(19, 33, 'coba', 'coba@gmail.com', '1999-12-12', 'bekasi', '0812', 'Female', 'img1653385672.png'),
+(20, 35, 'Thania', 'thania@gmail.com', '2022-05-11', 'medan', '0989283', 'Female', 'img1653983395.jpeg'),
+(24, NULL, 'sadfas', 'asdfa@gmail.com', '2022-06-17', '2asfas', '1231', 'Male', 'img1654420338.png');
 
 -- --------------------------------------------------------
 
@@ -197,7 +222,7 @@ CREATE TABLE `tbl_rinci_jadwal` (
 --
 
 INSERT INTO `tbl_rinci_jadwal` (`id_rinci`, `kode_jadwal`, `id_peserta`, `id_paket`) VALUES
-(25, '20220523PBXAC', 17, 9);
+(34, '20220605ZIQSU', 17, 8);
 
 -- --------------------------------------------------------
 
@@ -221,9 +246,15 @@ CREATE TABLE `tbl_user` (
 --
 
 INSERT INTO `tbl_user` (`id_user`, `nama_user`, `email`, `image`, `password`, `is_active`, `level_user`, `date_created`) VALUES
-(2, 'Feri', 'admin@gmail.com', 'img1652896114.jpg', 'shaniindira', 1, 1, 1652624175),
-(28, 'joko', 'joko@gmail.com', 'default.jpg', '12345', 1, 2, 1653316453),
-(29, 'Pamali', 'pamali@gmail.com', 'default.jpg', '12345', 1, 3, 1653317138);
+(2, 'Feri R', 'admin@gmail.com', 'img1654426326.jpg', 'admin', 1, 1, 1652624175),
+(28, 'Yoko', 'joko@gmail.com', 'img1654242273.jpeg', '12345', 1, 2, 1653316453),
+(29, 'Ahmad J', 'pamali@gmail.com', 'img1654319004.jpg', 'shani', 1, 3, 1653317138),
+(30, 'Robert', 'robert@gmail.com', 'default.jpg', '12345', 1, 3, 1653377981),
+(31, 'Budi', 'budi@gmail.com', 'default.jpg', '12345', 1, 3, NULL),
+(32, 'akun', 'akun@gmail.com', 'default.jpg', '12345', 1, 2, 1653383944),
+(33, 'coba', 'coba@gmail.com', 'default.jpg', '12345', 1, 2, 1653385610),
+(34, 'Peserta', 'peserta@gmail.com', 'img1654333565.jpeg', '12345', 1, 2, 1653447005),
+(35, 'thania', 'thania@gmail.com', 'img1654242495.jpg', '12345', 2, 2, 1653982436);
 
 --
 -- Indexes for dumped tables
@@ -246,6 +277,12 @@ ALTER TABLE `tbl_instruktur`
 --
 ALTER TABLE `tbl_jadwal`
   ADD PRIMARY KEY (`id_jadwal`);
+
+--
+-- Indexes for table `tbl_kantor`
+--
+ALTER TABLE `tbl_kantor`
+  ADD PRIMARY KEY (`id_kantor`);
 
 --
 -- Indexes for table `tbl_mobil`
@@ -291,43 +328,49 @@ ALTER TABLE `tbl_bank`
 -- AUTO_INCREMENT for table `tbl_instruktur`
 --
 ALTER TABLE `tbl_instruktur`
-  MODIFY `id_instruktur` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_instruktur` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `tbl_jadwal`
 --
 ALTER TABLE `tbl_jadwal`
-  MODIFY `id_jadwal` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_jadwal` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `tbl_kantor`
+--
+ALTER TABLE `tbl_kantor`
+  MODIFY `id_kantor` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_mobil`
 --
 ALTER TABLE `tbl_mobil`
-  MODIFY `id_mobil` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_mobil` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tbl_paket`
 --
 ALTER TABLE `tbl_paket`
-  MODIFY `id_paket` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_paket` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `tbl_peserta`
 --
 ALTER TABLE `tbl_peserta`
-  MODIFY `id_peserta` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_peserta` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `tbl_rinci_jadwal`
 --
 ALTER TABLE `tbl_rinci_jadwal`
-  MODIFY `id_rinci` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_rinci` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
